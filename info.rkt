e6357eaa296b840807e1964019353b125a4e232e
Danny Yoo <dyoo@cs.wpi.edu>
2012-02-13 20:51:20 -0500

    info.rkt

---
+++ info.rkt
@@ -0,0 +1,13 @@
+#lang setup/infotab
+(define name "least-squares: fitting a line to a sequence of 2d-points")
+(define categories '(misc))
+(define can-be-loaded-with 'all)
+(define required-core-version "5.1.1")
+(define version "1.0")
+(define repositories '("4.x"))
+(define scribblings '(("manual.scrbl")))
+(define primary-file "main.rkt")
+(define blurb 
+  '("least-squares: fitting a line to a sequence of 2d-points."))
+(define release-notes
+  '((p "First release.")))
